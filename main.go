79b40a16e6fe0d13aeff86a2015246e30a663554
HalfHedgehog <1041541387@qq.com>
2022-04-01 11:08:48 +0800

    1

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"GoHomeWork_week2/metadata"
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+
+	ch1 := make(chan int, 1)
+	ch2 := make(chan int, 1)
+
+	go func() {
+		time.Sleep(5 * time.Second)
+
+		metadata.MetaData.Source = "我已经被修改"
+
+		ch1 <- 1
+	}()
+
+	go func() {
+		fmt.Println(metadata.MetaData.Source)
+		ch2 <- 2
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case <-ch1:
+			fmt.Println("goroutine update is Done")
+		case <-ch2:
+			fmt.Println("goroutine select is Done")
+		}
+	}
 }
---
+++ metadata/metadata.go
@@ -0,0 +1,7 @@
+package metadata
+
+type CreditScoreMetadata struct {
+	Source string //数据来源（测试/生产）
+}
+
+var MetaData CreditScoreMetadata
